53c2b61e7496d7fe6a50a3824a64300515bdbe94
Heri Roque <heriroque.22@gmail.com>
2024-04-30 20:03:16 -0500

    Fix typo in ej4.py

--- ejercicios/ej4.py
+++ ejercicios/ej4.py
@@ -4,6 +4,6 @@ suma de sus componentes
 '''
 
 num = input("Ingrese un numero de 2 digitos: ")
-# Se extraen los digitos del numero ya que es un string, el cual tiene indices como un array y los combertimos a enteros en interger para sumarlos
+# Se extraen los digitos del numero ya que es un string, el cual tiene indices como un array y los convertimos a enteros en interger para sumarlos
 sum = int(num[0]) + int(num[1])
 print(f"La suma de los digitos es: {sum}")
